0e05ffe154b13d598324983f7fada86675730734
jinnybrain <jinnybrain@gmail.com>
2022-04-14 16:12:10 +0900

    feat(UI) : 탭바 > 계정화면 연결

--- lib/tab_page.dart
+++ lib/tab_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/account_page.dart';
 import 'package:instagram_clone/home_page.dart';
 
 class TabPage extends StatefulWidget {
@@ -14,7 +15,7 @@ class _TabPageState extends State<TabPage> {
   List _pages = [
     HomePage(),
     Text('Page2'),
-    Text('Page3')
+    AccountPage()
   ];
 
   @override
